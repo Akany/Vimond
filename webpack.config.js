3bf67f4ea8a0975cec3c08be1b91022d3451736a
Bogdan <sif.mori@gmail.com>
2017-03-12 16:32:48 +0200

    dev webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './app/main.js'
+    },
+
+    devtool: 'source-map',
+
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            {
+                test: /.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader', options: {sourceMap: true} },
+                    { loader: 'sass-loader', options: {sourceMap: true} }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve('app/index.html')
+        })
+    ]
+};
